de7143ea5e969f0003abbec872b52d0e97c1b0a8
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-04-09 15:28:24 +0900

    time

--- simtimer/ViewController.swift
+++ simtimer/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer = Timer()
+    var cunt = 0
+    @IBOutlet weak var timelabel: UILabel!
+    @IBOutlet weak var timeswitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @objc func updateTimer(){
+    let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EEE"
+        timelabel.text = formatter.string(from: date)
+        
+    }
+    
+    
+    @IBAction func timeswitch(_ sender: Any) {
+        if timeswitch.inOn == true {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }else{
+            timer.invalidate()
+        }
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
